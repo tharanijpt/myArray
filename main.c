63176ea54cc3faa279137509741bf263b939a34d
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2024-12-02 15:18:06 +0530

    Fix segfault

--- main.c
+++ main.c
@@ -12,15 +12,15 @@ int main(){
     int arr_size = 0;
     int ele_need = 0;
     printf("Enter the size of the array:");
-    scanf("%d",arr_size);
+    scanf("%d",&arr_size);
     printf("Enter the total elements:");
-    scanf("%d",ele_need);
+    scanf("%d",&ele_need);
 
     arr.A = (int *)malloc(arr_size * sizeof(int));
 
     for(int i=0;i<ele_need;i++){
         printf("Enter A[%d]=",i);
-        scanf("%d",arr.A[i]);
+        scanf("%d",&arr.A[i]);
     }
 
     arr.length = ele_need;
